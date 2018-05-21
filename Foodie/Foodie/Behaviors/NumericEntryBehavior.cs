5c64c84bc239f7a99c2c2ceb3a36712306ebb678
Poulad <poulad.pld@gmail.com>
2018-05-21 19:34:16 -0400

    Use behaviors

---
+++ Foodie/Foodie/Behaviors/NumericEntryBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+
+namespace Foodie.Behaviors
+{
+    class NumericEntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                return;
+            }
+
+            if (!double.TryParse(e.NewTextValue, out double _))
+            {
+                var entry = (Entry)sender;
+                entry.Text = e.OldTextValue;
+            }
+        }
+    }
+}
