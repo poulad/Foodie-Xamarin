0c7ebf8f34138ed1bed4411d3429800d2ada09a7
Poulad <poulad.pld@gmail.com>
2018-05-21 20:08:05 -0400

    Use Triggers

---
+++ Foodie/Foodie/Triggers/RequiredValidationTriggerAction.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Foodie.Triggers
+{
+    class RequiredValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            entry.BackgroundColor = string.IsNullOrEmpty(entry.Text)
+                ? Color.FromHex("#FFCDD2")
+                : Color.Default;
+        }
+    }
+}
